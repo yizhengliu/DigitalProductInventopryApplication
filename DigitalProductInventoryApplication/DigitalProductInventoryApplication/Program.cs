0f76f3e098bc2f27971fb02518a596e94562769c
JiheiBot <2757174746@qq.com>
2023-12-30 20:51:36 +0000

    Finish up Inventory development
    Explored how to add multi constrains for multiclass generic methods and use linq to implement the factory design pattern. briefly used developer command prompt of MSIL(microsoft intermediate language) to check how .net managed to booost the performance of using generics handle both value types and reference types at runtime. and how it gives compile time rules

---
+++ DigitalProductInventoryApplication/DigitalProductInventoryApplication/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalProductInventoryApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<ProductBase> products = new List<ProductBase>();
+            List<CategoryBase> categories = new List<CategoryBase>();
+
+            ProductBase digitalBook = FactoryPattern<DigitalBook, ProductBase>.GetInstance();
+
+            AddPropertiesToProduct(digitalBook, 1, "The Old Man and the Sea", 1);
+
+            products.Add(digitalBook);
+
+            ProductBase movie = FactoryPattern<Movie, ProductBase>.GetInstance();
+
+            AddPropertiesToProduct(movie, 2, "Highlander", 2);
+
+            products.Add(movie);
+
+            movie = FactoryPattern<Movie, ProductBase>.GetInstance();
+
+            AddPropertiesToProduct(movie, 3, "Shawshank Redemption", 2);
+
+            products.Add(movie);
+
+            ProductBase album = FactoryPattern<MusicRecording, ProductBase>.GetInstance();
+
+            AddPropertiesToProduct(album, 4, "Iron Man Soundtrack", 3);
+
+            products.Add(album);
+
+            CategoryBase digitalBookCategory = FactoryPattern<DigitalBookCategory, CategoryBase>.GetInstance();
+
+            AddPropertiesToCategory(digitalBookCategory, 1, "Book", "Books digitised for download");
+
+            categories.Add(digitalBookCategory);
+
+            CategoryBase movieCategory = FactoryPattern<MovieCategory, CategoryBase>.GetInstance();
+
+            AddPropertiesToCategory(movieCategory, 2, "Movie", "Movies digitised for download");
+
+            categories.Add(movieCategory);
+
+            CategoryBase musicCategory = FactoryPattern<MusicCategory, CategoryBase>.GetInstance();
+
+            AddPropertiesToCategory(musicCategory, 3, "Music", "Music digitised for download");
+
+            categories.Add(musicCategory);
+
+            var queryResults = GetProducts(products, categories);
+
+
+            foreach (var result in queryResults)
+            {
+                Console.WriteLine($"Product Id: {result.ProductId}");
+                Console.WriteLine($"Title: {result.Title}");
+                Console.WriteLine($"Category: {result.Category}");
+                Console.WriteLine($"Category Description: {result.CategoryDescription}");
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+        private static IEnumerable<ProductViewModel> GetProducts(List<ProductBase> products, List<CategoryBase> categories)
+        {
+
+            return from p in products
+                   join c in categories on p.CategoryId equals c.Id
+                   select new ProductViewModel
+                   {
+                       ProductId = p.Id,
+                       Title = p.Title,
+                       Category = c.Title,
+                       CategoryDescription = c.Description
+                   };
+
+        }
+
+        private static void AddPropertiesToCategory(CategoryBase category, int id, string title, string description)
+        {
+            category.Id = id;
+            category.Title = title;
+            category.Description = description;
+        }
+
+        private static void AddPropertiesToProduct(ProductBase product, int id, string title, int categoryId)
+        {
+            product.Id = id;
+            product.Title = title;
+            product.CategoryId = categoryId;
+
+        }
+    }
+
+    public class ProductViewModel 
+    {
+        public int ProductId {  get; set; }
+        public string Title {  get; set; }
+        public string Category { get; set; }
+        public string CategoryDescription {  get; set; }
+    }
+
+    public interface IPrimiaryProperties 
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+    }
+
+    public abstract class ProductBase : IPrimiaryProperties
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+
+    public abstract class CategoryBase : IPrimiaryProperties 
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class Movie : ProductBase 
+    {
+        public string Director { get; set; }
+        public string Producer {  get; set; }
+    }
+
+    public class DigitalBook : ProductBase 
+    {
+        public string Author {  get; set; }
+    }
+
+    public class MusicRecording : ProductBase 
+    {
+        public string RecordCompany {  get; set; }
+    }
+
+    public class MovieCategory : CategoryBase 
+    {
+        
+    }
+
+    public class DigitalBookCategory : CategoryBase
+    {
+
+    }
+
+    public class MusicCategory : CategoryBase
+    {
+
+    }
+
+    //abstract the construction of objects of varying types
+    //class means T will be a reference, U means it will be derived from U class, new() means that it has a construct method that is parameterless
+    public static class FactoryPattern<T, U> where T: class,U,new()
+                                             where U: class,IPrimiaryProperties
+    {
+        public static U GetInstance() 
+        {
+            U objT;
+            objT = new T();
+            return objT;
+        }
+    }
+}
